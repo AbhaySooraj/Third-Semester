69c43120741a86656207db88f6f6447709c3bd28
AbhaySooraj <94735651+AbhaySooraj@users.noreply.github.com>
2022-12-11 15:37:41 +0530

    Update exp2.cpp

--- OOP/LAB/exp2.cpp
+++ OOP/LAB/exp2.cpp
@@ -1,17 +1,29 @@
 #include<iostream>
 using namespace std;
+static int f1=1,f2=1;
 
 class student
 {
 	public:
-		int stdid=0,sph,sb1ia,sb2ia;
-		string sname,semail;
+		int stdid,sph,sb1ia,sb2ia,sb3ia,sb1e,sb2e,sb3e,sb1a,sb2a,sb3a;
+		float total,cgpa;
+		string sname,semail,ests;
 		
 		student()
 		{
-			stdid=stdid+count;
-			count=count+1;
-			
+			stdid=f1;
+			f1=f1+1;
+			sb1ia=0;
+			sb2ia=0;
+			sb3ia=0;
+			sb1e=0;
+			sb2e=0;
+			sb3e=0;
+			sb1a=0;
+			sb2a=0;
+			sb3a=0;
+			total=0;
+			cgpa=0;
 		}
 		
 		void getdatas()
@@ -19,14 +31,12 @@ class student
 			
 			cout<<" Enter name of student :";
 			cin>>sname;
-			cout<<endl;
-			
+
 			cout<<" Enter phone of student:";
-			cin>>ph;
-			cout<<endl;
+			cin>>sph;
 			
 			cout<<" Enter email of student:";
-			cin>>email;
+			cin>>semail;
 			cout<<endl;
 			
 			
@@ -34,89 +44,191 @@ class student
 		
 		void displaycards()
 		{
-			cout<<" Student ID :"<<stdid;
-			cout<<" Name of Student :"<<sname;
-			cout<<" Phone of Student :"<<sph;
-			cout<<" Email of Student :"<<semail;
-			cout<<" Subject1_IA:";
-			
-			cout<<"  :"<<stdid;
-			
-			
-			
-			
-		void printdatas()
-		{
-			cout<<" RollNo of student :"<<rno<<endl;
-			
-			cout<<" Name of student :"<<name<<endl;
-			
+			cout<<endl<<endl;
+			cout<<" Student ID :"<<stdid<<endl;
+			cout<<" Name of Student :"<<sname<<endl;
+			cout<<" Phone of Student :"<<sph<<endl;
+			cout<<" Email of Student :"<<semail<<endl;
+			cout<<" Subject1_IA :"<<sb1ia<<endl;
+			cout<<" Subject2_IA :"<<sb2ia<<endl;
+			cout<<" Subject3_IA :"<<sb3ia<<endl;
+			cout<<" Subject1_ESE :"<<sb1e<<endl;
+			cout<<" Subject2_ESE :"<<sb2e<<endl;
+			cout<<" Subject3_ESE :"<<sb3e<<endl;
+			cout<<" Subject1_Attendance :"<<sb1a<<endl;
+			cout<<" Subject2_Attendance :"<<sb2a<<endl;
+			cout<<" Subject3_Attendance :"<<sb3a<<endl;
+			cout<<" Total :"<<total<<endl;
+			cout<<" CGPA :"<<cgpa<<endl;
+			cout<<" Eligibility Status :"<<ests<<endl;
 		}
+	
+		friend class cteacher;
+		friend class steacher;
 };
 
-class teacher
+class cteacher
+{
+	public:
+		int empid,cph;
+		string cname,cemail;
+		
+		cteacher()
+		{
+			empid=f2;
+			f2=f2+1;
+		}
+		
+		void getdatac()
+		{
+			
+			cout<<" Enter name of class teacher:";
+			cin>>cname;
+			
+			cout<<" Enter phone of teacher:";
+			cin>>cph;
+			
+			cout<<" Enter email of teacher:";
+			cin>>cemail;
+			cout<<endl;
+		}
+		
+		void calculatetotal(student &t)
+		{
+			t.total=(t.sb1ia+t.sb2ia+t.sb3ia+t.sb1e+t.sb2e+t.sb3e)/6;
+		}
+		
+		void calculatecgpa(student &t)
+		{
+			t.cgpa=t.total*0.1;
+		} 
+		
+		void eligibility(student &t)
+		{
+			float sum=0;
+			sum=(t.sb1a+t.sb2a+t.sb3a)/3;
+			if(sum<80)
+			{
+				t.ests="False";
+			}
+			else
+			{
+				t.ests="True";
+			}
+		}
+			
+};
+
+class steacher
 {
 	public:
 		int empid,tph;
 		string tname,temail;
 		
-		teacher()
+		steacher()
 		{
-			empid=empid+count;
-			count=count+1;
+			empid=f2;
+			f2=f2+1;
 		}
 		
-		void inputteacher()
+		void getdatat()
 		{
 			
-			cout<<" Enter name of student :";
+			cout<<" Enter name of subject teacher:";
 			cin>>tname;
-			cout<<endl;
-			
-			cout<<" Enter phone of student:";
+
+			cout<<" Enter phone of subject teacher:";
 			cin>>tph;
-			cout<<endl;
 			
-			cout<<" Enter email of student:";
+			cout<<" Enter email of subject teacher:";
 			cin>>temail;
 			cout<<endl;
 		}
+		
+		void entermarks(student &t,int empids)
+		{
+			if(empids==1)
+			{
+				cout<<" Enter subject1_IA:";
+				cin>>t.sb1ia;
+		
+				cout<<" Enter subject1_EA:";
+				cin>>t.sb1e;
+			}
+			else if(empids==2)
+			{
+				cout<<" Enter subject2_IA:";
+				cin>>t.sb2ia;
+
+				cout<<" Enter subject2_EA:";
+				cin>>t.sb2e;
+			}
 			
+			else if(empids==3)
+			{
+				cout<<" Enter subject3_IA:";
+				cin>>t.sb3ia;
+				
+				cout<<" Enter subject3_EA:";
+				cin>>t.sb3e;
+			}
+		}
+		
+		void enteratt(student &t,int empids)
+		{
+			if(empids==1)
+			{
+				cout<<" Enter attendance of subject1 :";
+				cin>>t.sb1a;
+			}
+			else if(empids==2)
+			{	
+				cout<<" Enter attendance of subject2 :";
+				cin>>t.sb2a;
+			}
+			else if(empids==3)
+			{
+				cout<<" Enter attendance of subject3 :";
+				cin>>t.sb3a;
+				cout<<endl;
+			}
+		}
+};
 
 
-
-
-
-
-
-
-
-
-
-
-
-			cout<<" Enter subject1_IA:";
-			cin>>sb1ia;
-			cout<<endl;
-			
-			cout<<" Enter subject2_IA:";
-			cin>>sb2ia;
-			cout<<endl;
-			
-			cout<<" Enter subject3_IA:";
-			cin>>sb3ia;
-			cout<<endl;
-			
-			cout<<" Enter subject1_EA:";
-			cin>>sb1ea;
-			cout<<endl;
-			
-			cout<<" Enter subject2_EA:";
-			cin>>sb2ea;
-			cout<<endl;
-			
-			cout<<" Enter subject3_IA:";
-			cin>>sb3ea;
-			cout<<endl;
-			
-			
+int main()
+{
+	int i,j;
+	student std[3];
+	steacher st[3];
+	cteacher ct;
+	
+	cout<<" Enter details of class teacher :"<<endl;
+	ct.getdatac();
+	
+	
+	for(i=0;i<3;i++)
+	{
+		cout<<" Enter details of subject teacher :"<<i+1<<endl;
+		st[i].getdatat();
+	}
+	
+	cout<<" Enter details of student :"<<endl;
+	for(i=0;i<3;i++)
+	{
+		std[i].getdatas();
+		for(j=0;j<3;j++)
+		{
+			st[j].entermarks(std[i],st[j].empid);
+			st[j].enteratt(std[i],st[j].empid);
+		}
+		ct.calculatetotal(std[i]);
+		ct.calculatecgpa(std[i]);
+		ct.eligibility(std[i]);
+	}
+	
+	for(i=0;i<3;i++)
+	{
+		std[i].displaycards();
+	}
+}	
