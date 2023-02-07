033d8311790ed0f42f4e84fea6005b7416bca841
AbhaySooraj <94735651+AbhaySooraj@users.noreply.github.com>
2023-02-07 13:41:41 +0530

    Update exp5_3.cpp

--- OOP/LAB/exp5_3.cpp
+++ OOP/LAB/exp5_3.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class student
 {
 	public:
-		int rollno,m1,m2,tot;
+		int rollno,m1,m2,m3,tot;
 		string name;
 		
 		student()
@@ -20,6 +20,9 @@ class student
 			
 			cout<<" Enter mark 2: ";
 			cin>>m2;
+			
+			cout<<" Enter mark 3: ";
+			cin>>m3;
 		}
 		
 		void total()
@@ -33,6 +36,7 @@ class student
 			cout<<" Name of student :"<<name<<endl;
 			cout<<" Mark 1 :"<<m1<<endl;
 			cout<<" Mark 2 :"<<m2<<endl;
+			cout<<" Mark 3 :"<<m3<<endl;
 			cout<<" Total :"<<tot<<endl;
 		}
 };
@@ -40,38 +44,51 @@ class student
 class minorcourse: public student
 {
 	public:
-		int mm1,mm2,mm3;
+		string mm1,mm2,mm3;
 		
 		minorcourse()
 		{
-			cout<<" Enter minor course mark1: ";
+			cout<<" Enter minor course 1: ";
 			cin>>mm1;
 			
-			cout<<" Enter minor course mark2: ";
+			cout<<" Enter minor course 2: ";
 			cin>>mm2;
 			
-			cout<<" Enter minor course mark3: ";
+			cout<<" Enter minor course 3: ";
 			cin>>mm3;
 		}
 		
 		void showdata()
 		{
-			cout<<" Minor Course Mark 1 :"<<mm1<<endl;
-			cout<<" Minor Course Mark 2 :"<<mm2<<endl;
-			cout<<" Minor Course Mark 3 :"<<mm3<<endl;
+			cout<<" Roll no: "<<rollno<<endl;
+			cout<<" Name: "<<name<<endl;
+			cout<<mm1<<" mark: "<<m1<<endl;
+			cout<<mm2<<" mark: "<<m2<<endl;
+			cout<<mm3<<" mark: "<<m3<<endl;
+			cout<<"Total : "<<tot<<endl;
 		}
 };
 			
 
 int main()
 {
+	cout<<endl<<"Student class entry"<<endl;
+	
 	student obj1;
 	obj1.total();
+	
+	cout<<endl<<"Minorcourse class entry"<<endl;
 	minorcourse obj2;
 	obj2.total();
-
+	
 	student *ptr;
-	ptr= &obj2;
-	ptr->showdata();	
+	ptr=&obj2;
+	
+	cout<<endl<<"Student class entry"<<endl;
+	obj1.showdata();
+	
+	cout<<endl<<"Pointer way entry"<<endl;
+	ptr->showdata();
+	
 }
 
